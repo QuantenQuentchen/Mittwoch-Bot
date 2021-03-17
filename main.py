c93fad2afab2ca7096dde0225e4e6eb847481765
QuantenQuentchen <68424527+QuantenQuentchen@users.noreply.github.com>
2021-03-17 10:24:53 +0100

    Just abusing Discord Nothing to see here

--- main.py
+++ main.py
@@ -15,113 +15,122 @@ def NullVoid():
 
 random.seed()
 Prefix = "M!"
-Channels = {776823258385088552: 803731340154503250, 701051127612964964: 806312041697509426}
-#  """""
-#  Channels[701051127612964964] = 806312041697509426  # Test Channel
-
-#  Lasties[776823258385088552] = False  # 803731340154503250  # Ghost Cave
-#  Lasties[701051127612964964] = False  # Test Channel
-#  """""
-pickle.dump(Channels, open("Channels.p", "wb"))
-#  pickle.dump(Lasties, open("Lasties.p", "wb"))
-#  """""
-Channels = pickle.load(open("Channels.p", "rb"))
-Lasties = pickle.load(open("Lasties.p", "rb"))
 TOKEN = pickle.load(open("Token.p", "rb"))
-print(Lasties)
+DATA = {}
 src = "Mittwoch/"
 client = commands.Bot(command_prefix=Prefix, description="Extrem wichtiger Bot für extrem wichtige Sachen!")
+Delemiter = ";:;"
 
 for files in os.walk(src):
     file_count = len(files)
 
 
+async def DisSafe(Object):
+    channel = client.get_channel(821116383713951756)
+    Returnstring = ""
+    if Object is not await DisLoad():
+        for Guilds, Data in Object.items():
+            Returnstring = Returnstring + str(Guilds) + Delemiter + str(Data["Channels"]) + Delemiter + str(
+                Data["Lasties"]) + "\n"
+        await channel.send(content=Returnstring)
+
+
+async def DisLoad():
+    ReturnObject = {}
+    channel = client.get_channel(821116383713951756)
+    messages = await channel.history(limit=200).flatten()
+    for message in messages:
+        if message.author == client.user:
+            try:
+                for Data in message.content.split("\n"):
+                    Datachunk = Data.split(Delemiter)
+                    ReturnObject[int(Datachunk[0])] = {"Channels": int(Datachunk[1]), "Lasties": Datachunk[2]}
+                return ReturnObject
+            except Exception as e:
+                print(e)
+
+
 @client.event
 async def on_ready():
     for guilds in client.guilds:
+        global Data
+        Data = await DisLoad()
+        #  global Data
         try:
-            print(f"Connected to {guilds.name}({guilds.id}) der Mittwochschannel ist: {Channels[guilds.id]}")
-            pickle.dump(Lasties, open("Lasties.p", "wb"))
-        except:
-            pass
-
-
-@client.event
-async def on_guild_join(guild):
-    return
-
+            print(f"Connected to {guilds.name}({guilds.id}) der Mittwochschannel ist: {Data[guilds.id]['Channels']}")
+            await DisSafe(Data)
+        except Exception as e:
+            print(e)
 
 
 @client.command()
 @commands.has_permissions(administrator=True)
 async def Mittwoch_Channel(ctx):
+    global Data
     try:
-        Mit_chl = client.get_channel(Channels[ctx.author.guild.id])
+        Mit_chl = client.get_channel(Data[ctx.author.guild.id]['Channels'])
         print(ctx.author.guild.id)
         await ctx.send(f"Der Mittwochschannel ist{Mit_chl.mention} \n"
-                   f"Wenn du ihn ändern möchtest schreib: {Prefix}Mittwoch_Change #(Channel)")
-    except:
-        pass
+                       f"Wenn du ihn ändern möchtest schreib: {Prefix}Mittwoch_Change #(Channel)")
+    except Exception as e:
+        print(e)
 
 
 @client.command()
 @commands.has_permissions(administrator=True)
 async def Mittwoch_Change(ctx):
+    global Data
     try:
         Channels[ctx.author.guild.id] = ctx.message.channel_mentions[0].id
-        Mit_chl = client.get_channel(Channels[ctx.author.guild.id])
+        Mit_chl = client.get_channel(Data[ctx.author.guild.id]['Channels'])
         await ctx.send(f"Der neue Mittwochschannel ist{Mit_chl.mention}")
-        pickle.dump(Channels, open("Channels.p", "wb"))
-    except:
-        pass
-
-
-@client.command()
-async def cmd(ctx, channel: discord.TextChannel):
-    await ctx.send(f"Here's your mentioned channel ID: {channel.id}")
+        await DisSafe(Data)
+    except Exception as e:
+        print(e)
 
 
 @tasks.loop(hours=2)
 async def Channel_Check():
+    global Data
     for guilds in client.guilds:
         print(f"{guilds.name} == {guilds.id}")
-        pickle.dump(Channels, open("Channels.p", "rb"))
+        await DisSafe(Data)
 
 
-@tasks.loop(minutes=5)
+@tasks.loop(minutes=60)
 async def Mittwoch_check():
     await client.wait_until_ready()
+    global Data
     global Channels
     global Lasties
     activity = discord.Game(name="Professionel am Existieren.")
     for guilds in client.guilds:
         try:
             print("checking...")
-            Lasties = pickle.load(open("Lasties.p", "rb"))
+            Data = await DisLoad()
             activity = discord.Game(name="Professionel am Existieren.")
-            print(f"der letzte Mittwoch war am :{Lasties[guilds.id]}")
-            print(1)
-            if time.strftime("%A") == "Wednesday" and Lasties[guilds.id] != time.strftime("%W,%Y"):
+            print(f"der letzte Mittwoch war am :{Data[guilds.id]['Lasties']}")
+            if time.strftime("%A") == "Wednesday" and Data[guilds.id]['Lasties'] != time.strftime("%W,%Y"):
                 try:
                     activity = discord.Game(name="mit seinem Penis.", type=3)
-                    channel = client.get_channel(Channels[guilds.id])
+                    channel = client.get_channel(Data[guilds.id]['Channels'])
                     print(f"{guilds.name}")
                 except Exception as e:
                     print(e)
                 try:
                     await channel.send(content="@everyone ES IST Mittwoch meine Kerle",
-                                   file=discord.File(f"Mittwoch/Mittwoch{random.randint(1, file_count)}.png"))
-                    print(guilds.name, Lasties[guilds.id])
-                    Lasties[guilds.id] = time.strftime("%W,%Y")
+                                       file=discord.File(f"Mittwoch/Mittwoch{random.randint(1, file_count)}.png"))
+                    print(guilds.name, Data[guilds.id]['Lasties'])
+                    Data[guilds.id]['Lasties'] = time.strftime("%W,%Y")
                     print(time.strftime("%W,%Y"))
-                    print(Lasties[guilds.id])
-                    pickle.dump(Lasties, open("Lasties.p", "wb"))
+                    print(Data[guilds.id]['Lasties'])
+                    await DisSafe(Data)
                 except AttributeError:
                     pass
             elif time.strftime("%A") != "Wednesday":
                 "Kein Mittwoch"
-                pickle.dump(Lasties, open("Lasties.p", "wb"))
-            print(f"Es wurde auf dem Server {guilds.name}({guilds.id}) am {Lasties[guilds.id]} Gemittwocht")
+                await DisSafe(Data)
+            print(f"Es wurde auf dem Server {guilds.name}({guilds.id}) am {Data[guilds.id]['Lasties']} Gemittwocht")
         except Exception as e:
             print(e)
     await client.change_presence(activity=activity)
