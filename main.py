97613def6518e8941ac7976c42d842cbd7aff004
QuantenQuentchen <68424527+QuantenQuentchen@users.noreply.github.com>
2021-04-22 21:33:32 +0200

    No Honestly Fuck Intents

--- main.py
+++ main.py
@@ -25,38 +25,13 @@ Channel = {
 src = "Mittwoch/"
 intents = discord.Intents().default()
 intents.members = True
-client = commands.Bot(command_prefix=Prefix, description="Extrem wichtiger Bot für extrem wichtige Sachen!", intents=intents)
+client = commands.Bot(command_prefix=Prefix, description="Extrem wichtiger Bot für extrem wichtige Sachen!",
+                      intents=intents)
 Delemiter = ";:;"
 role_name = "Mittwoch⠀Boii"
 
 for files in os.walk(src):
     file_count = len(files)
-
-"""
-async def DisSafe(Object):
-    channel = client.get_channel(821116383713951756)
-    Returnstring = ""
-    if Object is not await DisLoad():
-        for Guilds, Data in Object.items():
-            Returnstring = Returnstring + str(Guilds) + Delemiter + str(Data["Channels"]) + Delemiter + str(
-                Data["Lasties"]) + "\n"
-        await channel.send(content=Returnstring)
-
-
-async def DisLoad():
-    ReturnObject = {}
-    channel = client.get_channel(821116383713951756)
-    messages = await channel.history(limit=200).flatten()
-    for message in messages:
-        if message.author == client.user:
-            try:
-                for Data in message.content.split("\n"):
-                    Datachunk = Data.split(Delemiter)
-                    ReturnObject[int(Datachunk[0])] = {"Channels": int(Datachunk[1]), "Lasties": Datachunk[2]}
-                return ReturnObject
-            except Exception as e:
-                print(e)
-"""
 
 
 @client.event
@@ -69,39 +44,11 @@ async def on_ready():
     print("Done")
 
 
-"""""
-@client.command()
-@commands.has_permissions(administrator=True)
-async def Mittwoch_Channel(ctx):
-    global Data
-    try:
-        Mit_chl = client.get_channel(Data[ctx.author.guild.id]['Channels'])
-        print(ctx.author.guild.id)
-        await ctx.send(f"Der Mittwochschannel ist{Mit_chl.mention} \n"
-                       f"Wenn du ihn ändern möchtest schreib: {Prefix}Mittwoch_Change #(Channel)")
-    except Exception as e:
-        print(e)
-
-
-@client.command()
-@commands.has_permissions(administrator=True)
-async def Mittwoch_Change(ctx):
-    global Data
-    try:
-        Channels[ctx.author.guild.id] = ctx.message.channel_mentions[0].id
-        Mit_chl = client.get_channel(Data[ctx.author.guild.id]['Channels'])
-        await ctx.send(f"Der neue Mittwochschannel ist{Mit_chl.mention}")
-        await DisSafe(Data)
-    except Exception as e:
-        print(e)
-"""""
-
-@tasks.loop(minutes=0)
+@tasks.loop(minutes=60)
 async def Mittwoch_check():
     await client.wait_until_ready()
     for guilds in client.guilds:
         Botmember = get(guilds.members, id=client.user.id)
-        print(Botmember.id)
         role = get(guilds.roles, name=role_name)
         if time.strftime("%A") == "Wednesday":
             if role in member.roles:
@@ -114,7 +61,6 @@ async def Mittwoch_check():
         else:
             await client.change_presence(activity=discord.Game(name="das ewige Wartespiel"))
             await Botmember.remove_roles(role, reason="Es ist nicht mehr Mittwoch.")
-
 
 
 Mittwoch_check.start()
